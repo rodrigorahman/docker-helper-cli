318aefa29c58f762e2f2de9d09fcd2e6fc789ea5
Rodrigo S. Rahman de Almeida <rodrigo.almeida@jfive.com.br>
2018-04-03 23:35:21 -0300

    Init project

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# docker-helper-cli
+# Configuracoes adicionais
+
+Para facilitar sua vida você pode utilizar algumas configurações extras para seu projeto, para isso basta criar o arquivo dhc-config.json
+
+Para dizer qual é a aplicação default para configurações como hotdeploy (hd) ou mesmo start com logs (s -l)
+
+```json
+{
+  "defaultWebProjectName" : "flightgroup-api",
+}
+```
+
+## Para adicionar a lista de escolhas no rebuild de um projeto
+
+Para isso basta adicionar as apps dentro do atributo appsEnabled
+
+```json
+{
+  "defaultWebProjectName" : "flightgroup-api",
+  "appsEnabled": [
+    {"name": "redis"},
+    {"name": "consul"}
+  ]
+}
+```
+
+## Criando seus proprios comandos 
---
+++ commands.js
@@ -0,0 +1,205 @@
+const commander  = require( 'commander');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const spawn = require('child_process').spawn;
+const { prompt } = require('inquirer');
+const fs =  require('fs');
+
+const configFileName = './dhc-config.json';
+const configExists = fs.existsSync(configFileName);
+
+let defaultWebProjectName = '';
+let appsEnableds = [
+  {name: "ALL", checked: true}
+];
+let customCommandExec = [];
+
+if(configExists){
+  
+  const config = JSON.parse(fs.readFileSync(configFileName, 'utf8'));
+  defaultWebProjectName = config.defaultWebProjectName;
+  
+  appsEnableds.push(...config.appsEnabled)
+  customCommandExec = config.customCommandExec;
+}
+
+appsEnableds.push({name: "Other"});
+console.log(appsEnableds)
+commander
+  .version('0.0.1')
+  .description('Utilitario para trabalhar com docker-compose em tempo de desenvolvimento, muitas pessoas utilizam o make para fazer isso, porem podemos deixar isso muito melhor utilizando nodejs');
+
+commander
+  .command('package')
+  .alias('p')
+  .description('Run mvn package para compilar todos os projetos que estao no pom.xml')
+  .action(async() => package())
+
+commander
+  .command('start')
+  .alias('s')
+  .option('-l, --log', 'tail in log after start')
+  .description('Run mvn package para compilar todos os projetos que estao no pom.xml e inicia o docker-compose')
+  .action(async(cmd) => {
+    await package();
+    await startStack()
+    if(cmd.log){
+      logApp(defaultWebProjectName)
+    }
+  })
+
+commander
+  .command('down')
+  .alias('d')
+  .description('Down Stack Development')
+  .action(stopStack)
+
+commander
+  .command('rebuild-app [app]')
+  .alias('r')
+  .description('Faz novamente o build do projeto especificado')
+  .action(async(app) => rebuildApp(app))
+
+commander
+  .command('log [app]')
+  .alias('l')
+  .description('Print logs')
+  .action(async(app) => {
+    if(!app){
+      app = defaultWebProjectName;
+    }
+    logApp(app)
+  })
+
+commander
+  .command('hot-deploy [app]')
+  .alias('hd')
+  .description('hot deploy app')
+  .action(async(app) => {
+    if(!app){
+      app = defaultWebProjectName;
+    }
+
+    await rebuildApp(app);
+    await logApp(app)
+  })
+console.log(customCommandExec.lenght);
+if(customCommandExec.length > 0){
+  customCommandExec.forEach(custom => {
+    commander
+      .command(custom.alias)
+      .description(custom.description)
+      .action(async() => {
+        custom.command.forEach(async(c) => await commandExec(c))
+      })
+  })
+}
+
+commander.parse(process.argv);
+
+async function package() {
+  await commandExec('mvn package') 
+}
+
+async function clean() {
+  await commandExec('mvn clean') 
+}
+
+async function startStack() {
+  await commandExec('docker-compose up --build -d')
+}
+
+async function stopStack() {
+  await commandExec('docker-compose down')
+}
+
+async function logApp(appName) {
+  try{
+    
+    let responses = {};
+    if(!appName){
+      responses = await prompt([
+        {
+          type: 'input',
+          name: 'app',
+          message: 'What the App would you like showing logs?'
+        }
+      ]);
+    }else{
+      responses.app = appName;
+    }
+    
+    let { app } = responses;
+
+    commandSpawn(`docker-compose logs -f ${app}`)
+    
+    
+  }catch(err) {
+    console.error(err)
+  }
+}
+
+async function rebuildApp(appName) {
+  try{
+    
+    let responses = {};
+    if(!appName){
+      responses = await prompt([
+        {
+          type: 'list',
+          name: 'app',
+          message: 'Would you like rebuild ALL or Other apps ?',
+          choices:  appsEnableds
+        }
+      ]);
+
+      if(responses.app === 'Other'){
+        responses = await prompt([
+          {
+            type: 'input',
+            name: 'app',
+            message: 'What the App name defined in docker compose ?'
+          }
+        ]);
+      }
+
+    }else{
+      responses.app = appName;
+    }
+    
+    let { app } = responses;
+
+    if(app === 'ALL') app = '';
+    
+    await commandExec(`docker-compose stop -t 1 ${app}`);
+    await clean();
+    await package();
+    await commandExec(`docker-compose build ${app}`);
+    await commandExec(`docker-compose create ${app}`);
+    await commandExec(`docker-compose start ${app}`);
+    
+  }catch(err) {
+    console.error(err)
+  }
+}
+
+async function commandExec(command) {
+  console.log(command);
+  let { err, stdout, stderr } = await exec(command);
+  printlog(err, stdout, stderr);
+}
+
+async function commandSpawn(command){
+  const commands = command.split(' ');
+  const p = spawn(commands[0], commands.slice(1));
+  p.stdout.setEncoding('utf8');
+  p.stdout.on('data', (data) => console.log(data.slice(0,-2)));
+}
+
+async function printlog(err, stdout, stderr){
+  if (err) {
+    console.error(stderr);
+    return;
+  }
+  console.log(stdout);
+}
